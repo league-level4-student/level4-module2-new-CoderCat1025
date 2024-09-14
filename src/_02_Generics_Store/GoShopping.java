571fd6a018ee74e388b059a50a6a571525dc347c
league <league@imac-34.attlocal.net>
2024-09-13 18:27:50 -0700

    to do: fix cart :(

--- src/_02_Generics_Store/GoShopping.java
+++ src/_02_Generics_Store/GoShopping.java
@@ -11,22 +11,31 @@ public class GoShopping {
 	
 	public static void main(String[] args) {
 		// 2. Create a Cart object of type Candy
-		
+//		Cart c = new Cart<Candy>();
 		// 3. Create another Cart object of type Cereal
-		
+//		Cart car = new Cart<Cereal>();
 		// 4. Add a few items to EACH cart
-		
-		
+//		c.add(new Candy());
+//		c.add(new Candy());
+//		c.add(new Candy());
+//		car.add(new Cereal());
+//		car.add(new Cereal());
+//		car.add(new Cereal());
 		
 		
 		// 5. Call the showCart() method on EACH cart
-		
+//		c.showCart();
+//		car.showCart();
 		
 		// 6. Run the code
 		
 		// 7. Comment out the code you have so far
 		
 		// 8. Repeat steps 2-5 for a Clothing Cart and a Toy Cart
+
+		Cart c = new Cart<Clothing>();
+		Cart car = new Cart<Toy>();
+		
 		
 		
 		
